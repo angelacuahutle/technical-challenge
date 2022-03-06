0339d5d161d47b56f36faef6d65943574074ecd0
angelacuahutle <angela.cuahutle@gmail.com>
2022-03-05 18:43:10 -0600

    Enable views in API

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -38,7 +38,7 @@ module Backend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
     Oj.optimize_rails
   end
 end
